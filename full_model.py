e703dee4c67dcc3c6ab6726aaf7e3bc369cb64a2
apmcleod <apmcleod@gmail.com>
2018-07-27 16:55:03 +0100

    Removed 1 hidden layer

--- full_model.py
+++ full_model.py
@@ -9,11 +9,9 @@ class Net(nn.Module):
         
         self.conv1 = nn.Conv1d(12, 32, 1)
         
-        self.fc1 = nn.Linear(32, 100)
-        self.fc2 = nn.Linear(100, 24)
+        self.fc1 = nn.Linear(32, 24)
         
     def forward(self, x):
         x = F.avg_pool1d(F.relu(self.conv1(x)), 300)
         x = x.view(-1, 32)
-        x = F.relu(self.fc1(x))
-        return F.softmax(self.fc2(x), dim=1)
+        return F.softmax(self.fc1(x), dim=1)
