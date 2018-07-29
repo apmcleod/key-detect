61807b6a82ba25eef9195f0a8465b012da2613af
apmcleod <apmcleod@gmail.com>
2018-07-29 14:53:10 +0100

    Updated score vector

--- keys.py
+++ keys.py
@@ -25,6 +25,8 @@ STRING_MAP = ['A\tmajor',
              'G\tminor',
              'G#\tminor']
 
+KEY_SUM = 1 + 0.5 + 0.5 + 0.3 + 0.2
+
 def get_vector_from_key(key):
     vector = np.zeros(24)
     if key == -1: # Unknown
@@ -32,15 +34,17 @@ def get_vector_from_key(key):
     if key < 12: #major
         vector[key] = 1
         vector[(key + 7) % 12] = 0.5
+        vector[(key + 5) % 12] = 0.5
         vector[(key + 9) % 12 + 12] = 0.3
         vector[key + 12] = 0.2
     else: # minor
         vector[key] = 1
         vector[(key + 7) % 12 + 12] = 0.5
+        vector[(key + 5) % 12 + 12] = 0.5
         vector[(key + 3) % 12] = 0.3
         vector[key - 12] = 0.2
         
-    vector /= 2
+    vector /= KEY_SUM
     
     return vector
 
