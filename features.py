99fd140ee56e1959cb733ab5d1879e2083cf469f
apmcleod <apmcleod@gmail.com>
2018-08-01 11:38:50 +0100

    Moved cqt to function

--- features.py
+++ features.py
@@ -24,10 +24,13 @@ def cqt():
         # X = np.append(X, X_shifted, axis=0)
 
         for song in X:
-            cqt = librosa.core.hybrid_cqt(song, sr=fileio.FS, bins_per_octave=24, n_bins=144, hop_length=int(fileio.FS / 5))
+            cqt = get_cqt(song)
             X_cqt = np.concatenate((X_cqt, cqt.reshape(1, cqt.shape[0], cqt.shape[1])), axis=0)
 
     np.savez_compressed('{}/X_cqt.npz'.format(fileio.WORKING_PREFIX), X=np.log(X_cqt + EPS))
+    
+def get_cqt(song):
+    return librosa.core.hybrid_cqt(song, sr=fileio.FS, bins_per_octave=24, n_bins=144, hop_length=int(fileio.FS / 5))
 
 if __name__ == '__main__':
     cqt()
