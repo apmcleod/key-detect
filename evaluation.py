d5803e54e7e41b4e253ed04fed205aedebe46ca2
kungfujam <james.f.owers@gmail.com>
2018-08-01 16:18:34 +0100

    Added a confusion matrix method and ran classification
    Logistic regression classification runs better than the funny
    linear regresson hack. Also, the confusion matrix revealed that
    our data has a minor key bias.

--- evaluation.py
+++ evaluation.py
@@ -1,5 +1,8 @@
 import keys
 import numpy as np
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 def get_scores(targets, guesses):
@@ -25,3 +28,25 @@ def get_scores(targets, guesses):
 def get_score_single(target, guess):
     scores = keys.get_vector_from_key(target) * keys.KEY_SUM
     return scores[guess]
+
+
+def plot_confusion_matrix(true, pred, classes=None, title='Confusion matrix', norm=False, **kwargs):
+    """Plots a confusion matrix."""
+    cm = confusion_matrix(true, pred)
+    heatmap_kwargs = dict(annot=True, fmt='d')
+    if norm:
+        cm_norm = cm/cm.sum(axis=1)[:, np.newaxis]
+        heatmap_kwargs['data'] = cm_norm
+        heatmap_kwargs['vmin']=0.
+        heatmap_kwargs['vmax']=1.
+        heatmap_kwargs['fmt']='.3f'
+    else:
+        heatmap_kwargs['data'] = cm
+    if classes is not None:
+        heatmap_kwargs['xticklabels']=classes
+        heatmap_kwargs['yticklabels']=classes
+    heatmap_kwargs.update(kwargs)
+    sns.heatmap(**heatmap_kwargs)
+    plt.title(title)
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
