6b9746cb5d2558c8514c431810768206dfb1755e
apmcleod <apmcleod@gmail.com>
2018-08-01 10:23:05 +0100

    Fixed cqt epsilon

--- features.py
+++ features.py
@@ -4,6 +4,8 @@ import numpy as np
 import librosa
 from glob import glob
 
+EPS = np.finfo(float).eps
+
 
 def cqt():
     print('Creating cqt features from chunked audio')
@@ -25,7 +27,7 @@ def cqt():
             cqt = librosa.core.hybrid_cqt(song, sr=fileio.FS, bins_per_octave=24, n_bins=144, hop_length=int(fileio.FS / 5))
             X_cqt = np.concatenate((X_cqt, cqt.reshape(1, cqt.shape[0], cqt.shape[1])), axis=0)
 
-    np.savez_compressed('{}/X_cqt.npz'.format(fileio.WORKING_PREFIX), X=np.log(X_cqt))
+    np.savez_compressed('{}/X_cqt.npz'.format(fileio.WORKING_PREFIX), X=np.log(X_cqt + EPS))
 
 if __name__ == '__main__':
     cqt()
