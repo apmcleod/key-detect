b70c6462f72d2a5ff7a27a410112ea550f4d5bb3
kungfujam <james.f.owers@gmail.com>
2018-08-10 13:40:37 +0100

    Added scheduler for MO2 as in paper

--- model_tester.py
+++ model_tester.py
@@ -16,18 +16,18 @@ import h5py
 
 def train_model(h5_file, net, model_name, batch_size=64, num_epochs=100,
                 optimizer=None, scheduler=None, log_path=None, device=torch.device("cpu"),
-                seed=None, print_every=1, save_every=10, resume=None, 
+                seed=None, print_every=1, save_every=10, resume=None,
                 small_ram=False):
-    
+
     if seed:
         torch.manual_seed(seed)
         np.random.seed(seed)
-    
+
     net.to(device)
-    
+
     X_test_full = None
     Y_test_full = None
-    
+
     data_file = h5py.File(h5_file, 'r')
     train_size = data_file['X_train'].shape[0]
     test_size = data_file['X_test'].shape[0]
@@ -37,7 +37,7 @@ def train_model(h5_file, net, model_name, batch_size=64, num_epochs=100,
         X_test_full = data_file['X_test'].value
         Y_test_full = data_file['Y_test'].value
     data_file.close()
-    
+
     num_batches = math.ceil(train_size / batch_size)
 
     criterion = nn.CrossEntropyLoss()
@@ -48,7 +48,7 @@ def train_model(h5_file, net, model_name, batch_size=64, num_epochs=100,
     best_score = 0
     global_train_losses = []
     global_test_losses = []
-    
+
     if resume:
         print("loading checkpoint '{}'".format(resume))
         checkpoint = torch.load(resume)
@@ -59,9 +59,7 @@ def train_model(h5_file, net, model_name, batch_size=64, num_epochs=100,
         global_test_losses = checkpoint['global_test_losses']
         global_train_losses = checkpoint['global_train_losses']
         optimizer.load_state_dict(checkpoint['optimizer'])
-        if scheduler:
-            scheduler.load_state_dict(checkpoint['scheduler'])
-       
+
     print(net)
 
     for epoch in range(epoch_start, num_epochs):
@@ -71,7 +69,7 @@ def train_model(h5_file, net, model_name, batch_size=64, num_epochs=100,
         else:
             loss_log_file = open('{}_loss.log'.format(log_path), "a")
             acc_log_file = open('{}_acc.log'.format(log_path), "a")
-        
+
         shuffle = np.arange(train_size)
         np.random.shuffle(shuffle)
 
@@ -81,10 +79,10 @@ def train_model(h5_file, net, model_name, batch_size=64, num_epochs=100,
         for batch_num in range(num_batches):
             if (batch_num + 1) % 50 == 0:
                 print('Batch {}/{}'.format(batch_num + 1, num_batches))
-                
+
             bottom = batch_size * batch_num
             top = min(batch_size * (batch_num + 1), train_size)
-            
+
             if small_ram:
                 indeces = sorted(shuffle[bottom : top])
                 data_file = h5py.File(h5_file, 'r')
@@ -112,26 +110,26 @@ def train_model(h5_file, net, model_name, batch_size=64, num_epochs=100,
 
         if scheduler:
             scheduler.step(test_score)
-        
+
         print("{},{}".format(avg_loss, test_loss), file=loss_log_file)
         print("{},{}".format(avg_score, test_score), file=acc_log_file)
         if log_path is not None:
             loss_log_file.close()
             acc_log_file.close()
-            
+
         is_best = False
         if test_score > best_score:
             best_score = test_score
             is_best = True
-            
+
         global_train_losses.append(avg_loss)
         global_test_losses.append(test_loss)
-            
+
         if epoch % print_every == 0:
             print('epoch ' + str(epoch) + ' loss: ' + str(avg_loss))
             print('Train accuracy = ' + str(avg_score))
             print('Test accuracy = ' + str(test_score))
-            
+
         if epoch % save_every == 0 or is_best:
             save_checkpoint({
                 'epoch': epoch + 1,
@@ -140,30 +138,29 @@ def train_model(h5_file, net, model_name, batch_size=64, num_epochs=100,
                 'global_test_losses': global_test_losses,
                 'global_train_losses': global_train_losses,
                 'optimizer' : optimizer.state_dict(),
-                'scheduler' : scheduler.state_dict() if scheduler else None
             }, is_best, model_name, 'checkpoint.pth.tar')
             if is_best:
                 filename = '{}_model_best.pkl'.format(model_name)
                 torch.save(net, '{}/{}'.format(fileio.OUTPUT_PREFIX, filename))
     print('Done')
-    
+
     return global_train_losses, global_test_losses
 
 
 def get_score_batched(net, h5_file, X_full, Y_full, criterion, small_ram=False,
                       device=torch.device("cpu"), batch_size=256, X='X_test', Y='Y_test'):
-    
+
     if small_ram:
         data_file = h5py.File(h5_file, 'r')
         X_size = data_file[X].shape[0]
         data_file.close()
     else:
         X_size = X_full.shape[0]
-    
+
     num_batches = math.ceil(X_size / batch_size)
     avg_score = 0
     avg_loss = 0
-    
+
     for batch_num in range(num_batches):
         bottom = batch_size * batch_num
         top = min(batch_size * (batch_num + 1), X_size)
@@ -176,45 +173,45 @@ def get_score_batched(net, h5_file, X_full, Y_full, criterion, small_ram=False,
         else:
             X_batch = torch.from_numpy(X_full[bottom : top]).float().to(device)
             Y_batch = torch.from_numpy(Y_full[bottom : top]).to(device)
-        
+
         Y_hat = net(X_batch)
         loss = criterion(Y_hat, Y_batch)
         loss.backward()
 
         avg_loss += loss.item() * X_batch.size()[0]
-        
+
         score = np.sum(evaluation.get_scores(Y_batch, np.argmax(Y_hat.data, axis=1)))
         avg_score += score
-        
+
     return avg_score / X_size, avg_loss / X_size
 
 
 def save_checkpoint(state, is_best, model_name, filename='checkpoint.pth.tar'):
     if not os.path.exists(fileio.OUTPUT_PREFIX):
         os.makedirs(fileio.OUTPUT_PREFIX)
-        
+
     torch.save(state, '{}/{}_{}'.format(fileio.OUTPUT_PREFIX, model_name, filename))
-    
+
     if is_best:
         shutil.copyfile('{}/{}_{}'.format(fileio.OUTPUT_PREFIX, model_name, filename),
                         '{}/{}_model_best.pth.tar'.format(fileio.OUTPUT_PREFIX, model_name))
-        
-        
+
+
 
 if __name__ == '__main__':
     import full_model
-    
+
     parser = argparse.ArgumentParser(description="")
     parser.add_argument("-m", "--model", help="Which model to use for prediction",
                         type=int, choices=range(1, 3), required=True)
     parser.add_argument("-s", "--seed", help="A seed to use for modelling",
                         type=int, default=None)
-    parser.add_argument("-c", "--cuda", help="Cuda device number to use", 
+    parser.add_argument("-c", "--cuda", help="Cuda device number to use",
                         type=str, default="0")
-    parser.add_argument("-n", "--num-epochs", help="Number of epochs to train", 
+    parser.add_argument("-n", "--num-epochs", help="Number of epochs to train",
                         type=int, default=100)
     args = parser.parse_args()
-    
+
     model = args.model
     if model == 1:
         model_name = 'MO1'
@@ -222,16 +219,16 @@ if __name__ == '__main__':
         model_name = 'MO2'
     os.environ["CUDA_VISIBLE_DEVICES"] = args.cuda
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    
+
     seed = args.seed
     if seed:
         torch.manual_seed(seed)
-    
+
     h5_file = 'data/working/data.h5'
 
     if model_name == 'MO2':
         net = full_model.ReproductionNet()
-        optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9, 
+        optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9,
                               weight_decay=0.0001)
         scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', factor=0.5, patience=10, threshold=0.0)
     else:
@@ -239,7 +236,7 @@ if __name__ == '__main__':
         net = full_model.ConvBiLstm()
         optimizer = optim.Adam(net.parameters(), lr=0.001, weight_decay=0.0001)
         scheduler = None
-    
+
     log_dir = 'data/output/logs'
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
@@ -248,11 +245,11 @@ if __name__ == '__main__':
         os.remove('{}_acc.log'.format(log_path))
     if os.path.isfile('{}_loss.log'.format(log_path)):
         os.remove('{}_loss.log'.format(log_path))
-    train_losses, test_losses = train_model(h5_file, net, model_name, 
+    train_losses, test_losses = train_model(h5_file, net, model_name,
                                             num_epochs=args.num_epochs,
-                                            optimizer=optimizer,
+                                            optimizer=optimizer, scheduler=scheduler,
                                             device=device, seed=seed,
                                             log_path=log_path)
-    
-    
-    
+
+
+
